91fe5544ff9b57aa63fc4b9f44145e71960b62f7
Nuno Sousa <nunosousa@users.noreply.github.com>
2020-01-31 13:17:33 +0000

    Trying to improve fiting function.

--- Source/TelescopeMirrorTester.py
+++ Source/TelescopeMirrorTester.py
@@ -2,7 +2,7 @@ from Mirror import Mirror
 import numpy as np
 import matplotlib.pyplot as pyplot
 
-test = 167
+test = 158
 
 mirror_name = "My Telescope Mirror"
 mirror_diameter = 203.5
@@ -27,10 +27,10 @@ if test == 167:
 if test == 1670:
     # Test data - Test 167
     r = np.array(
-        [0.0, 49.14, 52.85, 55.22, 61.06, 65.42, 68.53, 72.53, 74.06, 76.96, 78.39, 79.91, 81.06, 82.16, 84.38, 85.83,
+        [49.14, 52.85, 55.22, 61.06, 65.42, 68.53, 72.53, 74.06, 76.96, 78.39, 79.91, 81.06, 82.16, 84.38, 85.83,
          87.21, 89.09, 90.55])
     f = np.array(
-        [0.0, 0.60, 0.70, 0.80, 0.90, 1.00, 1.10, 1.20, 1.30, 1.40, 1.45, 1.50, 1.55, 1.60, 1.65, 1.70, 1.75, 1.80,
+        [0.60, 0.70, 0.80, 0.90, 1.00, 1.10, 1.20, 1.30, 1.40, 1.45, 1.50, 1.55, 1.60, 1.65, 1.70, 1.75, 1.80,
          1.85])
 
 my_telescope_mirror = Mirror(mirror_name, mirror_diameter, mirror_radius_of_curvature)
