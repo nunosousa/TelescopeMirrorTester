7a2c7121bc1c6cf90af155a21f1bc1d277319166
Nuno Sousa <nunosousa@users.noreply.github.com>
2023-01-02 00:37:50 +0000

    Update arduino_test_sketch.ino

--- Tester/Firmware/arduino_test_sketch/arduino_test_sketch.ino
+++ Tester/Firmware/arduino_test_sketch/arduino_test_sketch.ino
@@ -13,9 +13,12 @@ const byte hBridgePWM1 = 11;
 const byte hBridgePWM2 = 3;
 
 // Current measurement pins
-const byte analogPin1 = A1;
-const byte analogPin2 = A2;
-const byte analogPin3 = A0;
+const int analogPin1 = A1;
+const int analogPin2 = A2;
+const int analogPin3 = A0;
+
+// PCA9535 interript pint flag
+char interruptFlag = 0;
 
 void setup() {
   // Initialize UART
@@ -37,7 +40,7 @@ void setup() {
   Wire.endTransmission();
 
   // Initialize interrupt pin
-  pinMode(interruptPin, INPUT_PULLUP);
+  pinMode(interruptPin, INPUT);
   attachInterrupt(digitalPinToInterrupt(interruptPin), printSwitch, FALLING);
 
   // Initialize h bridge enable pins as outputs and set their initial values
@@ -53,13 +56,21 @@ void setup() {
   digitalWrite(hBridgePWM1, LOW);
   pinMode(hBridgePWM2, OUTPUT);
   digitalWrite(hBridgePWM2, LOW);
+
+  // Initialize analoge input pins
+  pinMode(A0, INPUT);
+  digitalWrite(A0, LOW);
+  pinMode(A1, INPUT);
+  digitalWrite(A1, LOW);
+  pinMode(A2, INPUT);
+  digitalWrite(A2, LOW);
 }
 
 void loop() {
-  int rawCurrentValue;
-  float CurrentValue;
+  int rawCurrentValue = 0;
+  float CurrentValue = 0.0;
   
-  // Toggle LEDs one by one
+  // ############################# Toggle LEDs one by one
 
   // LED 0 () on
   Wire.beginTransmission(0x20);   // begins a transmission to PCA9535 on address 0x20
@@ -123,153 +134,141 @@ void loop() {
   Wire.write(0x0F);               // Set outputs on port 0 (LEDs)
   Wire.endTransmission();
 
+  // #############################
   // Test the motors in both directions, one by one, and measure the current
   // H-bridge 1 direction a
   digitalWrite(hBridgeEn1, HIGH);
   digitalWrite(hBridgePWM1, HIGH);
   digitalWrite(hBridgePWM2, LOW);
-
-  delay(500);
+  delay(1000);
 
   // Measure current
   rawCurrentValue = analogRead(analogPin1);
-  CurrentValue = rawCurrentValue * 20.0;
+  CurrentValue = (rawCurrentValue / 20.0) / 0.25;
   Serial.println("Current on H-bridge 1, direction a");
   Serial.println(CurrentValue);
-
-  delay(500);
+  delay(1000);
 
   // H-bridge 1 direction b
   digitalWrite(hBridgeEn1, HIGH);
   digitalWrite(hBridgePWM1, LOW);
   digitalWrite(hBridgePWM2, HIGH);
-
-  delay(500);
+  delay(1000);
 
   // Measure current
   rawCurrentValue = analogRead(analogPin1);
-  CurrentValue = rawCurrentValue * 20.0;
+  CurrentValue = (rawCurrentValue / 20.0) / 0.25;
   Serial.println("Current on H-bridge 1, direction b");
   Serial.println(CurrentValue);
-
-  delay(500);
+  delay(1000);
 
   // H-bridge 1 disable
   digitalWrite(hBridgeEn1, LOW);
   digitalWrite(hBridgePWM1, LOW);
   digitalWrite(hBridgePWM2, LOW);
-
-  delay(500);
-
-  // Measure current
-  rawCurrentValue = analogRead(analogPin2);
-  CurrentValue = rawCurrentValue * 20.0;
-  Serial.println("Current on H-bridge 2, direction a");
-  Serial.println(CurrentValue);
-
-  delay(500);
+  delay(1000);
 
   // H-bridge 2 direction a
   digitalWrite(hBridgeEn2, HIGH);
   digitalWrite(hBridgePWM1, HIGH);
   digitalWrite(hBridgePWM2, LOW);
-
-  delay(500);
+  delay(1000);
 
   // Measure current
   rawCurrentValue = analogRead(analogPin2);
-  CurrentValue = rawCurrentValue * 20.0;
-  Serial.println("Current on H-bridge 2, direction b");
+  CurrentValue = (rawCurrentValue / 20.0) / 0.25;
+  Serial.println("Current on H-bridge 2, direction a");
   Serial.println(CurrentValue);
-
-  delay(500);
+  delay(1000);
 
   // H-bridge 2 direction b
   digitalWrite(hBridgeEn2, HIGH);
   digitalWrite(hBridgePWM1, LOW);
   digitalWrite(hBridgePWM2, HIGH);
-
-  delay(500);
+  delay(1000);
 
   // Measure current
   rawCurrentValue = analogRead(analogPin2);
-  CurrentValue = rawCurrentValue * 20.0;
+  CurrentValue = (rawCurrentValue / 20.0) / 0.25;
   Serial.println("Current on H-bridge 2, direction b");
   Serial.println(CurrentValue);
-
-  delay(500);
+  delay(1000);
 
   // H-bridge 2 disable
   digitalWrite(hBridgeEn2, LOW);
   digitalWrite(hBridgePWM1, LOW);
   digitalWrite(hBridgePWM2, LOW);
-  
   delay(1000);
 
   // H-bridge 3 direction a
   digitalWrite(hBridgeEn3, HIGH);
   digitalWrite(hBridgePWM1, HIGH);
   digitalWrite(hBridgePWM2, LOW);
-
-  delay(500);
+  delay(1000);
 
   // Measure current
   rawCurrentValue = analogRead(analogPin3);
-  CurrentValue = rawCurrentValue * 20.0;
+  CurrentValue = (rawCurrentValue / 20.0) / 0.25;
   Serial.println("Current on H-bridge 3, direction a");
   Serial.println(CurrentValue);
-
-  delay(500);
+  delay(1000);
 
   // H-bridge 3 direction b
   digitalWrite(hBridgeEn3, HIGH);
   digitalWrite(hBridgePWM1, LOW);
   digitalWrite(hBridgePWM2, HIGH);
-
-  delay(500);
+  delay(1000);
 
   // Measure current
   rawCurrentValue = analogRead(analogPin3);
-  CurrentValue = rawCurrentValue * 20.0;
+  CurrentValue = (rawCurrentValue / 20.0) / 0.25;
   Serial.println("Current on H-bridge 3, direction b");
   Serial.println(CurrentValue);
-
-  delay(500);
+  delay(1000);
 
   // H-bridge 3 disable
   digitalWrite(hBridgeEn3, LOW);
   digitalWrite(hBridgePWM1, LOW);
   digitalWrite(hBridgePWM2, LOW);
-}
 
-void printSwitch() {
-  char pinState;
+  // #############################
+  // Check for switch interrupt and print which one was pressed
+  if(interruptFlag != 0)
+  {
+    Serial.println("Interrupt activated");
+    interruptFlag = 0;  // Clear flag
+  }
+  else
+  {
+    Serial.println("Interrupt not activated");
+  }
+  
+  if(digitalRead(interruptPin) == HIGH)
+  {
+    Serial.println("Interrupt pin high");
+  }
+  else
+  {
+    Serial.println("Interrupt pin low");
+  }
+
+  char pinState1 = 0, pinState2 = 0, pinState3 = 0;
 
   // Read IO switch pins
   Wire.beginTransmission(0x20);   // begins a transmission to PCA9535 on address 0x20
   Wire.write(0x00);               // Input port 0 address
-  pinState = Wire.read();         // Get input port 0 bit status
+  Wire.requestFrom(0x20, 2);
+  pinState1 = Wire.read();
+  pinState2 = Wire.read();
+  pinState3 = Wire.read();
   Wire.endTransmission();
 
-  switch (pinState)
-  {
-    case 0x01:
-      Serial.println("LIM_SW00 pressed");
-      break;
-    case 0x02:
-      Serial.println("LIM_SW01 pressed");
-      break;
-    case 0x04:
-      Serial.println("LIM_SW02 pressed");
-      break;
-    case 0x08:
-      Serial.println("LIM_SW03 pressed");
-      break;
-    case 0x10:
-      Serial.println("LIM_SW04 pressed");
-      break;
-    case 0x20:
-      Serial.println("LIM_SW05 pressed");
-      break;
-  }
+  Serial.println("Switch input state:");
+  Serial.println(pinState1, BIN);
+  Serial.println(pinState2, BIN);
+  Serial.println(pinState3, BIN);
+}
+
+void printSwitch() {
+  interruptFlag = 1;
 }
