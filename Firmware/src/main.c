16111b8819f0f1382b6c8c6341998a298ce09802
Nuno Sousa <sousa.nms@gmail.com>
2023-03-06 00:05:17 +0000

    error fixing

--- Firmware/src/main.c
+++ Firmware/src/main.c
@@ -1,4 +1,20 @@
-int main(void)
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define BLINK_DELAY_MS 5000
+
+int main (void)
 {
-    while(1);
+  // Arduino digital pin 13 (pin 5 of PORTB) for output
+  DDRB |= 0B100000; // PORTB5
+
+  while(1) {
+    // turn LED on
+    PORTB |= 0B100000; // PORTB5
+    _delay_ms(BLINK_DELAY_MS);
+
+    // turn LED off
+    PORTB &= ~ 0B100000; // PORTB5
+    _delay_ms(BLINK_DELAY_MS);
+  }
 }
