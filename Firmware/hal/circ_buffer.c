f406505d7e0679bb1a3856b6a6f44fee3b2f674d
Nuno Sousa <nunosousa@users.noreply.github.com>
2023-03-16 12:59:43 +0000

    Update circ_buffer.c

--- Firmware/hal/circ_buffer.c
+++ Firmware/hal/circ_buffer.c
@@ -4,20 +4,20 @@ typedef struct circular_buffer
     void *buffer_end; // end of data buffer
     size_t capacity;  // maximum number of items in the buffer
     size_t count;     // number of items in the buffer
-    size_t sz;        // size of each item in the buffer
+    size_t item_size; // size of each item in the buffer
     void *head;       // pointer to head
     void *tail;       // pointer to tail
 } circular_buffer;
 
-void cb_init(circular_buffer *cb, size_t capacity, size_t sz)
+void cb_init(circular_buffer *cb, size_t capacity, size_t item_size, void *buffer)
 {
-    cb->buffer = malloc(capacity * sz);
+    cb->buffer = buffer;
     if(cb->buffer == NULL)
         // handle error
-    cb->buffer_end = (char *)cb->buffer + capacity * sz;
+    cb->buffer_end = (char *)cb->buffer + capacity * item_size;
     cb->capacity = capacity;
     cb->count = 0;
-    cb->sz = sz;
+    cb->item_size = item_size;
     cb->head = cb->buffer;
     cb->tail = cb->buffer;
 }
@@ -27,11 +27,23 @@ void cb_push(circular_buffer *cb, const void *item)
     if(cb->count == cb->capacity){
         // handle error
     }
-    memcpy(cb->head, item, cb->sz);
-    cb->head = (char*)cb->head + cb->sz;
+    memcpy(cb->head, item, cb->item_size);
+    cb->head = (char*)cb->head + cb->item_size;
     if(cb->head == cb->buffer_end)
         cb->head = cb->buffer;
     cb->count++;
+}
+
+void cb_undo_push(circular_buffer *cb, void *item);
+{
+    if(cb->count == 0){
+        // handle error
+    }
+    cb->head = (char*)cb->head - cb->item_size;
+    if(cb->head == cb->buffer)
+        cb->head = cb->buffer_end;
+    memcpy(item, cb->head, cb->item_size);
+    cb->count--;
 }
 
 void cb_pop(circular_buffer *cb, void *item)
@@ -39,8 +51,8 @@ void cb_pop(circular_buffer *cb, void *item)
     if(cb->count == 0){
         // handle error
     }
-    memcpy(item, cb->tail, cb->sz);
-    cb->tail = (char*)cb->tail + cb->sz;
+    memcpy(item, cb->tail, cb->item_size);
+    cb->tail = (char*)cb->tail + cb->item_size;
     if(cb->tail == cb->buffer_end)
         cb->tail = cb->buffer;
     cb->count--;
