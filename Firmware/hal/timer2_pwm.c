dfb155dfb20566b69d2054983e3cb4aa8b7ae20d
Nuno Sousa <sousa.nms@gmail.com>
2023-03-27 00:15:04 +0100

    progressed pwm

--- Firmware/hal/timer2_pwm.c
+++ Firmware/hal/timer2_pwm.c
@@ -18,5 +18,24 @@ void timer2_pwm_init(void)
     PORTB &= ~(1 << PORTB3);
     PORTD &= ~(1 << PORTD3);
 
-    
+    // Set Timer1 to fast PWM mode with TOP at 0xFF
+    TCCR1A |= (1 << WGM10) | (1 << WGM11);
+    TCCR1B |= (1 << WGM12) | (1 << WGM13);
+
+    // Set prescaler to 64, resulting in 976.5625 Hz PWM frequency
+    TCCR1B |= (1 << CS11) | (1 << CS10);
+
+    // Set PB3 and PD3 to non-inverted PWM mode
+    TCCR1A |= (1 << COM1A1) | (1 << COM1B1);
+}
+
+void timer2_pwm_set_duty_cycle(int duty_cycle_a, int duty_cycle_b)
+{
+    // Convert duty cycles from percentage (0-100) to OCR1A/OCR1B values (0-255)
+    int ocr1a_value = (duty_cycle_a * 255) / 100;
+    int ocr1b_value = (duty_cycle_b * 255) / 100;
+
+    // Set OCR1A and OCR1B to the calculated values
+    OCR1A = ocr1a_value;
+    OCR1B = ocr1b_value;
 }
--- Firmware/hal/timer2_pwm.h
+++ Firmware/hal/timer2_pwm.h
@@ -2,5 +2,6 @@
 #define TIMER2_PWM_H
 
 void timer2_pwm_init(void);
+void timer2_pwm_set_duty_cycle(int duty_cycle_a, int duty_cycle_b);
 
 #endif /* TIMER2_PWM_H */
