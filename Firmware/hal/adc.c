8a8d98adfed0e7cb073918030c40a681499cb57e
Nuno Sousa <sousa.nms@gmail.com>
2023-04-07 16:33:54 +0100

    progressed adc code

--- Firmware/hal/adc.c
+++ Firmware/hal/adc.c
@@ -5,6 +5,9 @@
 
 #define THRESHOLD 512 // set threshold to 2.5V (assuming Vref=5V)
 
+/*
+ * tbd
+ */
 ISR(ADC_vect)
 {
     uint16_t adc_val = ADC; // Read ADC value
@@ -30,6 +33,43 @@ void adc_init(void)
 
     /* Enable general interrupts after setup */
     sei();
+
+    /* Default option - set source to Temperature sensor */
+    adc_select_analog_input(TEMP);
+}
+
+/*
+ * tbd
+ */
+void adc_select_analog_input(adc_input_t input)
+{
+    switch (input)
+    {
+    case ADC0: /* Set input channel to ADC0. */
+        ADMUX &= ~(_BV(MUX0) | _BV(MUX1) | _BV(MUX2) | _BV(MUX3));
+        break;
+    case ADC1: /* Set input channel to ADC1. */
+        ADMUX &= ~(_BV(MUX1) | _BV(MUX2) | _BV(MUX3));
+        ADMUX |= _BV(MUX0);
+        break;
+    case ADC2: /* Set input channel to ADC2. */
+        ADMUX &= ~(_BV(MUX0) | _BV(MUX2) | _BV(MUX3));
+        ADMUX |= _BV(MUX1);
+        break;
+    case TEMP: /* Set input channel to ADC8 - Temperature Sensor. */
+        ADMUX &= ~(_BV(MUX0) | _BV(MUX1) | _BV(MUX2));
+        ADMUX |= _BV(MUX3);
+        break;
+    case BG_1V1: /* Set input channel to 1.1V (V_BG) */
+        ADMUX &= ~_BV(MUX0);
+        ADMUX |= _BV(MUX1) | _BV(MUX2) | _BV(MUX3);
+        break;
+    default: /* Set input channel to 0V (GND) */
+        ADMUX |= _BV(MUX0) | _BV(MUX1) | _BV(MUX2) | _BV(MUX3);
+        break;
+    }
+
+    return;
 }
 
 /*
--- Firmware/hal/adc.h
+++ Firmware/hal/adc.h
@@ -1,9 +1,24 @@
 #ifndef ADC_H
 #define ADC_H
 
+typedef enum
+{
+    ADC0,
+    ADC1,
+    ADC2,
+    TEMP,
+    BG_1V1,
+    GND
+} adc_input_t;
+
 /*
  * tbd
  */
 void adc_init(void);
+
+/*
+ * tbd
+ */
+void adc_select_analog_input(adc_input_t input);
 
 #endif /* ADC_H */
