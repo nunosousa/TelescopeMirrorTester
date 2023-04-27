7c59bde4cd19c9ce8fcc7f9187d82e64a2353edf
Nuno Sousa <sousa.nms@gmail.com>
2023-04-27 19:06:51 +0100

    progressed code

--- Firmware/src/ir_remote.c
+++ Firmware/src/ir_remote.c
@@ -14,10 +14,14 @@
 void ir_remote_process(void)
 {
     motor_parameters_t *motor_state = NULL;
-    static motor_t motorID;
+    static motor_t motorID = NONE;
+    motor_drive_t drive;
     nec_command_t *remote_code;
     uint16_t code;
-    uint8_t speed;
+    int16_t speed;
+
+    /* Turn ir remote indicator led on */
+    indicator_led_set_state(REMOTE, LED_ON);
 
     /* Get the last received IR remote code */
     remote_code = nec_ir_get_command();
@@ -30,104 +34,92 @@ void ir_remote_process(void)
     /* Parse code */
     switch (code)
     {
-    case 0x0045: /* key 1 - motor A */
-        /* Turn ir remote indicator led on */
-        indicator_led_set_state(REMOTE, LED_ON);
-
+    case 0x0045: /* key 1 - select motor A */
         motorID = MOTOR_A;
-        printf("1 key\n");
-
-        /* Turn ir remote indicator led off */
-        indicator_led_set_state(REMOTE, LED_OFF);
-
         break;
 
-    case 0x0046: /* key 2 - motor B */
-        /* Turn ir remote indicator led on */
-        indicator_led_set_state(REMOTE, LED_ON);
-
+    case 0x0046: /* key 2 - select motor B */
         motorID = MOTOR_B;
-        printf("2 key\n");
-
-        /* Turn ir remote indicator led off */
-        indicator_led_set_state(REMOTE, LED_OFF);
-
         break;
 
-    case 0x0047: /* key 3 - motor B */
-        /* Turn ir remote indicator led on */
-        indicator_led_set_state(REMOTE, LED_ON);
-
+    case 0x0047: /* key 3 - select motor B */
         motorID = MOTOR_C;
-        printf("3 key\n");
+        break;
 
-        /* Turn ir remote indicator led off */
-        indicator_led_set_state(REMOTE, LED_OFF);
+    case 0x0018: /* key up - increase speed on selected motor */
+        if (motorID == NONE)
+            break;
+
+        motor_state = get_motor_state(motorID);
+        if (motor_state == NULL)
+            break;
+
+        speed = motor_state->speed;
+        drive = motor_state->drive;
+
+        /* Reflect direction on the speed sign for ease of calculations */
+        if (drive == REVERSE_DRIVE)
+            speed = -speed;
+
+        /* Update speed */
+        speed += MOTOR_SPEED_STEP;
+
+        /* Avoid abrupt changes in direction */
+        if ((drive == REVERSE_DRIVE) && (speed > 0))
+        {
+            drive = BRAKE;
+            speed = 0;
+        }
+
+        /* Update drive direction and speed signal if required */
+        if (speed > 0)
+            drive = FORWARD_DRIVE;
+        else if (speed < 0)
+        {
+            drive = REVERSE_DRIVE;
+            speed = -speed;
+        }
+
+        if (speed > motor_state->max_speed)
+            speed = motor_state->max_speed;
+
+        motor_drive(motorID, drive, (uint8_t)speed);
 
         break;
 
-    case 0x0018: /* key up - increase speed */
-        /* Turn ir remote indicator led on */
-        indicator_led_set_state(REMOTE, LED_ON);
+    case 0x0052: /* key down - decrease speed on selected motor */
+        if (motorID == NONE)
+            break;
 
         motor_state = get_motor_state(motorID);
-        if (motor_state != NULL)
-        {
-            if (motor_state->speed > (motor_state->max_speed - MOTOR_SPEED_STEP))
-                speed = motor_state->max_speed;
-            else
-                speed = motor_state->speed + MOTOR_SPEED_STEP;
+        if (motor_state == NULL)
+            break;
 
-            motor_drive(motorID, FORWARD_DRIVE, speed);
+        if (motor_state->speed < (motor_state->min_speed + MOTOR_SPEED_STEP))
+            speed = motor_state->min_speed;
+        else
+            speed = motor_state->speed - MOTOR_SPEED_STEP;
 
-            printf("up key\n");
-        }
-
-        /* Turn ir remote indicator led off */
-        indicator_led_set_state(REMOTE, LED_OFF);
+        motor_drive(motorID, FORWARD_DRIVE, speed);
 
         break;
 
-    case 0x0052: /* key down - decrease speed */
-        /* Turn ir remote indicator led on */
-        indicator_led_set_state(REMOTE, LED_ON);
+    case 0x001C: // /* key OK - set speed to zero on selected motor */
+        if (motorID == NONE)
+            break;
 
         motor_state = get_motor_state(motorID);
-        if (motor_state != NULL)
-        {
-            if (motor_state->speed < (motor_state->min_speed + MOTOR_SPEED_STEP))
-                speed = motor_state->min_speed;
-            else
-                speed = motor_state->speed - MOTOR_SPEED_STEP;
+        if (motor_state == NULL)
+            break;
 
-            motor_drive(motorID, FORWARD_DRIVE, speed);
-
-            printf("down key\n");
-        }
-
-        /* Turn ir remote indicator led off */
-        indicator_led_set_state(REMOTE, LED_OFF);
-
-        break;
-
-    case 0x001C: // /* key OK - set speed to zero */
-        /* Turn ir remote indicator led on */
-        indicator_led_set_state(REMOTE, LED_ON);
-
-        motor_state = get_motor_state(motorID);
-        if (motor_state != NULL)
-        {
-            motor_drive(motorID, FORWARD_DRIVE, 0);
-
-            printf("OK key\n");
-        }
-
-        /* Turn ir remote indicator led off */
-        indicator_led_set_state(REMOTE, LED_OFF);
+        motor_drive(motorID, BRAKE, 0);
 
         break;
 
     default:
         break;
     }
+
+    /* Turn ir remote indicator led off */
+    indicator_led_set_state(REMOTE, LED_OFF);
 }
