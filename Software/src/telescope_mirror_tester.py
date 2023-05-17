ebe4299d77e3da3e8ba1b12aae5b5f91118f2778
Nuno Sousa <sousa.nms@gmail.com>
2023-05-17 23:36:41 +0100

    progressed gui

--- Software/src/telescope_mirror_tester.py
+++ Software/src/telescope_mirror_tester.py
@@ -1,20 +1,66 @@
-from tkinter import *
-from tkinter.ttk import *
+import tkinter as tk
+import tkinter.ttk as ttk
 
-# writing code needs to
-# create the main window of
-# the application creating
-# main window object named root
-root = Tk()
+# https://realpython.com/python-gui-tkinter/
 
-# giving title to the main window
-root.title("First_Program")
+root = tk.Tk()
 
-# Label is what output will be
-# show on the window
-label = Label(root, text ="Hello World !").pack()
+root.title("Telescope Mirror Tester")
 
-# calling mainloop method which is used
-# when your application is ready to run
-# and it tells the code to keep displaying
+# Position controls
+frm_pos = tk.LabelFrame(master=root, text="Axis Motor Speed Controls")
+
+# X Axis controls
+frm_x = tk.LabelFrame(master=frm_pos, text="X axis, Speed: ")
+
+btn_x_mm = ttk.Button(master=frm_x, text="<<", width=3)
+btn_x_m = ttk.Button(master=frm_x, text="<", width=3)
+btn_x_stop = ttk.Button(master=frm_x, text="STOP", width=5)
+btn_x_p = ttk.Button(master=frm_x, text=">", width=3)
+btn_x_pp = ttk.Button(master=frm_x, text=">>", width=3)
+
+btn_x_mm.grid(row=0, column=0, padx=4, pady=4)
+btn_x_m.grid(row=0, column=1, padx=4, pady=4)
+btn_x_stop.grid(row=0, column=2, padx=4, pady=4)
+btn_x_p.grid(row=0, column=3, padx=4, pady=4)
+btn_x_pp.grid(row=0, column=4, padx=4, pady=4)
+
+frm_x.grid(row=0, column=0, padx=4, pady=4)
+
+# Y Axis controls
+frm_y = tk.LabelFrame(master=frm_pos, text="Y axis, Speed: ")
+
+btn_y_mm = ttk.Button(master=frm_y, text="<<", width=3)
+btn_y_m = ttk.Button(master=frm_y, text="<", width=3)
+btn_y_stop = ttk.Button(master=frm_y, text="STOP", width=5)
+btn_y_p = ttk.Button(master=frm_y, text=">", width=3)
+btn_y_pp = ttk.Button(master=frm_y, text=">>", width=3)
+
+btn_y_mm.grid(row=0, column=0, padx=4, pady=4)
+btn_y_m.grid(row=0, column=1, padx=4, pady=4)
+btn_y_stop.grid(row=0, column=2, padx=4, pady=4)
+btn_y_p.grid(row=0, column=3, padx=4, pady=4)
+btn_y_pp.grid(row=0, column=4, padx=4, pady=4)
+
+frm_y.grid(row=1, column=0, padx=4, pady=4)
+
+# Z Axis controls
+frm_z = tk.LabelFrame(master=frm_pos, text="Z axis, Speed: ")
+
+btn_z_mm = ttk.Button(master=frm_z, text="<<", width=3)
+btn_z_m = ttk.Button(master=frm_z, text="<", width=3)
+btn_z_stop = ttk.Button(master=frm_z, text="STOP", width=5)
+btn_z_p = ttk.Button(master=frm_z, text=">", width=3)
+btn_z_pp = ttk.Button(master=frm_z, text=">>", width=3)
+
+btn_z_mm.grid(row=0, column=0, padx=4, pady=4)
+btn_z_m.grid(row=0, column=1, padx=4, pady=4)
+btn_z_stop.grid(row=0, column=2, padx=4, pady=4)
+btn_z_p.grid(row=0, column=3, padx=4, pady=4)
+btn_z_pp.grid(row=0, column=4, padx=4, pady=4)
+
+frm_z.grid(row=2, column=0, padx=4, pady=4)
+
+frm_pos.pack()
+
 root.mainloop()
